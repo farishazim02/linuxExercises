4f298523ee05047f7de68d41696a64ce45f6fa27
Wenyu Dai <wdai57@wisc.edu>
2025-03-04 09:16:40 -0600

    done

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# this sh removes all files in a directory larget than n bytes
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+
+directory=$1
+bytes=$2
+
+find $directory -type f -size +${bytes}c -exec rm {} ";"
